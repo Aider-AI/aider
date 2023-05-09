350ba4574f5b7cecd6a4eb24b9d098b63b0f0bcd
Paul Gauthier <paul@geomagical.com>
2023-05-09 07:35:01 -0700

    Initial commit: Added new files to the git repo: test_utils.py

---
+++ test_utils.py
@@ -0,0 +1,15 @@
+import unittest
+from utils import replace_most_similar_chunk
+
+class TestUtils(unittest.TestCase):
+    def test_replace_most_similar_chunk(self):
+        whole = "This is a sample text.\nAnother line of text.\nYet another line."
+        part = "sample text"
+        replace = "replaced text"
+        expected_output = "This is a replaced text.\nAnother line of text.\nYet another line."
+
+        result = replace_most_similar_chunk(whole, part, replace)
+        self.assertEqual(result, expected_output)
+
+if __name__ == "__main__":
+    unittest.main()
