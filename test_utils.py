17589d5241e8395b62f0762f0100bfb1ba03299a
Paul Gauthier <paul@geomagical.com>
2023-05-09 07:36:07 -0700

    Add test case for `replace_most_similar_chunk` with multi-line non-perfect match.
    # Context:
    USER: add another test case for replace_most_similar_chunk where it's not a perfect match, and part has more than 1 line
    ASSISTANT: I will add a new test case to `test_utils.py` where the `part` has more than 1 line and it's not a perfect match.
    
    ```python
    test_utils.py
    <<<<<<< ORIGINAL
    if __name__ == "__main__":
        unittest.main()
    =======
        def test_replace_most_similar_chunk_not_perfect_match(self):
            whole = "This is a sample text.\nAnother line of text.\nYet another line."
            part = "sample text.\nAnother line"
            replace = "replaced text.\nModified line"
            expected_output = "This is a replaced text.\nModified line of text.\nYet another line."
    
            result = replace_most_similar_chunk(whole, part, replace)
            self.assertEqual(result, expected_output)
    
    if __name__ == "__main__":
        unittest.main()
    >>>>>>> UPDATED
    ```
    
    This new test case checks if the `replace_most_similar_chunk` function can handle a non-perfect match with a multi-line `part`.

--- test_utils.py
+++ test_utils.py
@@ -11,5 +11,14 @@ class TestUtils(unittest.TestCase):
         result = replace_most_similar_chunk(whole, part, replace)
         self.assertEqual(result, expected_output)
 
+    def test_replace_most_similar_chunk_not_perfect_match(self):
+        whole = "This is a sample text.\nAnother line of text.\nYet another line."
+        part = "sample text.\nAnother line"
+        replace = "replaced text.\nModified line"
+        expected_output = "This is a replaced text.\nModified line of text.\nYet another line."
+
+        result = replace_most_similar_chunk(whole, part, replace)
+        self.assertEqual(result, expected_output)
+
 if __name__ == "__main__":
     unittest.main()
