09fbbc0dae6bbb3ffc5111e2622f28ea0f7796fa
Paul Gauthier <paul@geomagical.com>
2023-05-11 17:39:15 -0700

    wip: Added import subprocess to test_main.py.

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,8 @@ import sys
 import tempfile
 from unittest import TestCase
 from aider.main import main
+import subprocess
+
 
 class TestMain(TestCase):
     def test_main_with_empty_dir_no_files_on_command(self):
@@ -23,8 +25,6 @@ class TestMain(TestCase):
                 main(['foo.txt'])
                 sys.stdin = save_stdin
                 self.assertTrue(os.path.exists('foo.txt'))
-
-    import subprocess
 
     def test_main_with_empty_git_dir_new_file(self):
         with tempfile.TemporaryDirectory() as temp_dir:
