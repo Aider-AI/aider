981710ca160b3a4d2892a3af07ca7659f96b17d4
Paul Gauthier <paul@geomagical.com>
2023-05-13 09:44:22 -0700

    Bumped version to 0.3.0 in setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="aider",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
