cf63fd31d5472d803acb0711293cff9302aa2d62
Paul Gauthier <aider@paulg.org>
2023-05-19 06:24:49 -0700

    no n/a

--- aider/ctags.py
+++ aider/ctags.py
@@ -1,15 +1,17 @@
 import json
 import sys
 
+
 def print_tags_info(filename):
     with open(filename, "r") as tags_file:
         for line in tags_file:
             tag = json.loads(line)
-            scope = tag.get("scope", "N/A")
-            kind = tag.get("kind", "N/A")
-            name = tag.get("name", "N/A")
-            signature = tag.get("signature", "N/A")
+            scope = tag.get("scope", "")
+            kind = tag.get("kind", "")
+            name = tag.get("name", "")
+            signature = tag.get("signature", "")
             print(scope, kind, name, signature)
+
 
 if __name__ == "__main__":
     for filename in sys.argv[1:]:
