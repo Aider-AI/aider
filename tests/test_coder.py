7e75cfb1fae951ec60888d7b315a1e2c613b5c16
Paul Gauthier <aider@paulg.org>
2023-05-27 08:29:05 -0700

    added missing import

--- tests/test_coder.py
+++ tests/test_coder.py
@@ -1,6 +1,8 @@
+import os
 import unittest
 from unittest.mock import MagicMock
 from aider.coder import Coder
+
 
 class TestCoder(unittest.TestCase):
     def test_check_for_file_mentions(self):
@@ -18,11 +20,12 @@ class TestCoder(unittest.TestCase):
         coder.repo = mock_repo
 
         # Call the check_for_file_mentions method
-        result = coder.check_for_file_mentions("Please check file1.txt and file2.py")
+        coder.check_for_file_mentions("Please check file1.txt and file2.py")
 
         # Check if coder.abs_fnames contains both files
         expected_files = {os.path.abspath("file1.txt"), os.path.abspath("file2.py")}
         self.assertEqual(coder.abs_fnames, expected_files)
+
 
 if __name__ == "__main__":
     unittest.main()
