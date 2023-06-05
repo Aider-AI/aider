2f684bf99ea966df3fa57391b9f1cc2cf997ff19
Paul Gauthier <aider@paulg.org>
2023-06-05 16:46:38 -0700

    cleanup

--- aider/diffs.py
+++ aider/diffs.py
@@ -19,27 +19,21 @@ def main():
 
     diff = list(difflib.ndiff(lines_orig, lines_updated))
 
-    # Mapping between the line numbers
-    mapping = {}
-
-    num_orig, num_updated = 0, 0
-    last_non_deleted_orig = 0
+    num_orig = 0
+    last_non_deleted_orig = None
 
     for line in diff:
-        print(f"{num_orig:2d} {num_updated:2d} {line}", end="")
+        # print(f"{num_orig:2d} {num_updated:2d} {line}", end="")
         code = line[0]
         if code == " ":
-            # lines are the same in both files
-            mapping[num_orig] = num_updated
             num_orig += 1
-            num_updated += 1
             last_non_deleted_orig = num_orig
         elif code == "-":
             # line only in file_orig
             num_orig += 1
         elif code == "+":
             # line only in file_updated
-            num_updated += 1
+            pass
 
     dump(last_non_deleted_orig)
 
