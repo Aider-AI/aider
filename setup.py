85a14309ca8e37f05d87898ca84059441020819d
Paul Gauthier <aider@paulg.org>
2023-06-07 13:53:04 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="aider",
-    version="0.4.0",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
