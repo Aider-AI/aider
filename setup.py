419fccfa8a71860b2180752a4c86ad2f5c921290
Paul Gauthier <aider@paulg.org>
2023-06-08 12:44:41 -0700

    wip

--- setup.py
+++ setup.py
@@ -7,19 +7,22 @@ import re
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
-    long_description = re.sub(r'\n!\[.*\]\(.*\)', '', long_description)
-    long_description = re.sub(r'\n- \[.*\]\(#.*\)', '', long_description)
+    long_description = re.sub(r"\n!\[.*\]\(.*\)", "", long_description)
+    long_description = re.sub(r"\n- \[.*\]\(#.*\)", "", long_description)
 
 setup(
-    name="aider-chat",
-    version="0.5.3",
+    name="aider-chat-2",
+    version="0.0.1",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=requirements,
     entry_points={
         "console_scripts": [
             "aider = aider.main:main",
         ],
     },
+    description="aider is GPT powered coding in your terminal",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/paul-gauthier/aider",
 )
