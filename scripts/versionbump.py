2b1fab43a167af27ec1c6da51333a84861f3a5b6
Paul Gauthier <aider@paulg.org>
2023-06-17 07:22:45 -0700

    aider: Added a `--dry-run` argument to the `scripts/versionbump.py` script to print each step without actually modifying the `__init__.py` file or running the git commands.
    # Context:
    USER: add a --dry-run arg that just prints each step, so it won't overwrite __init__.py or actually rnu the git cmds
    ASSISTANT: I will add a `--dry-run` argument to the `scripts/versionbump.py` script. When this argument is provided, the script will print each step without actually modifying the `__init__.py` file or running the git commands.
    
    Here are the changes:
    
    ```python
    scripts/versionbump.py
    <<<<<<< ORIGINAL
    def main():
        parser = argparse.ArgumentParser(description="Bump version")
        parser.add_argument("new_version", help="New version in x.y.z format")
    =======
    def main():
        parser = argparse.ArgumentParser(description="Bump version")
        parser.add_argument("new_version", help="New version in x.y.z format")
        parser.add_argument("--dry-run", action="store_true", help="Print each step without actually executing them")
    >>>>>>> UPDATED
    
    scripts/versionbump.py
    <<<<<<< ORIGINAL
        args = parser.parse_args()
    =======
        args = parser.parse_args()
        dry_run = args.dry_run
    >>>>>>> UPDATED
    
    scripts/versionbump.py
    <<<<<<< ORIGINAL
        with open("aider/__init__.py", "w") as f:
            f.write(updated_content)
    =======
        if dry_run:
            print("Updating aider/__init__.py with new version")
        else:
            with open("aider/__init__.py", "w") as f:
                f.write(updated_content)
    >>>>>>> UPDATED
    
    scripts/versionbump.py
    <<<<<<< ORIGINAL
        subprocess.run(["git", "add", "aider/__init__.py"], check=True)
        subprocess.run(["git", "commit", "-m", f"version bump to {new_version}"], check=True)
        subprocess.run(["git", "tag", f"v{new_version}"], check=True)
        subprocess.run(["git", "push", "origin"], check=True)
        subprocess.run(["git", "push", "origin", f"v{new_version}"], check=True)
    =======
        git_commands = [
            ["git", "add", "aider/__init__.py"],
            ["git", "commit", "-m", f"version bump to {new_version}"],
            ["git", "tag", f"v{new_version}"],
            ["git", "push", "origin"],
            ["git", "push", "origin", f"v{new_version}"],
        ]
    
        for cmd in git_commands:
            if dry_run:
                print(f"Running: {' '.join(cmd)}")
            else:
                subprocess.run(cmd, check=True)
    >>>>>>> UPDATED
    ```
    
    Now you can run the script with the `--dry-run` argument to see the steps without actually executing them:
    
    ```bash
    python scripts/versionbump.py x.y.z --dry-run
    ```

--- scripts/versionbump.py
+++ scripts/versionbump.py
@@ -7,7 +7,9 @@ from packaging import version
 def main():
     parser = argparse.ArgumentParser(description="Bump version")
     parser.add_argument("new_version", help="New version in x.y.z format")
+    parser.add_argument("--dry-run", action="store_true", help="Print each step without actually executing them")
     args = parser.parse_args()
+    dry_run = args.dry_run
 
     new_version = version.parse(args.new_version)
     if not isinstance(new_version, version.Version):
@@ -22,14 +24,25 @@ def main():
 
     updated_content = re.sub(r'__version__ = ".+?"', f'__version__ = "{new_version}"', content)
 
-    with open("aider/__init__.py", "w") as f:
-        f.write(updated_content)
+    if dry_run:
+        print("Updating aider/__init__.py with new version")
+    else:
+        with open("aider/__init__.py", "w") as f:
+            f.write(updated_content)
 
-    subprocess.run(["git", "add", "aider/__init__.py"], check=True)
-    subprocess.run(["git", "commit", "-m", f"version bump to {new_version}"], check=True)
-    subprocess.run(["git", "tag", f"v{new_version}"], check=True)
-    subprocess.run(["git", "push", "origin"], check=True)
-    subprocess.run(["git", "push", "origin", f"v{new_version}"], check=True)
+    git_commands = [
+        ["git", "add", "aider/__init__.py"],
+        ["git", "commit", "-m", f"version bump to {new_version}"],
+        ["git", "tag", f"v{new_version}"],
+        ["git", "push", "origin"],
+        ["git", "push", "origin", f"v{new_version}"],
+    ]
+
+    for cmd in git_commands:
+        if dry_run:
+            print(f"Running: {' '.join(cmd)}")
+        else:
+            subprocess.run(cmd, check=True)
 
 if __name__ == "__main__":
     main()
