bc38fcde65afb526587f069bc33c5190ba611215
Paul Gauthier <aider@paulg.org>
2023-06-20 16:39:57 -0700

    aider: Called superclass's `__init__` method before setting the `gpt_prompts` attribute in `EditBlockCoder`.
    # Context:
    USER: call super
    ASSISTANT: To call the superclass's `__init__` method in the `EditBlockCoder` class, you need to modify the `aider/coders/editblock.py` file. Here's the change:
    
    ```python
    aider/coders/editblock.py
    <<<<<<< ORIGINAL
    class EditBlockCoder(Coder):
        def __init__(self, *args, **kwargs):
            self.gpt_prompts = EditBlockPrompts()
    =======
    class EditBlockCoder(Coder):
        def __init__(self, *args, **kwargs):
            super().__init__(*args, **kwargs)
            self.gpt_prompts = EditBlockPrompts()
    >>>>>>> UPDATED
    ```
    
    This change calls the superclass's `__init__` method before setting the `gpt_prompts` attribute.

--- aider/coders/editblock.py
+++ aider/coders/editblock.py
@@ -4,4 +4,5 @@ from .base import Coder
 
 class EditBlockCoder(Coder):
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.gpt_prompts = EditBlockPrompts()
