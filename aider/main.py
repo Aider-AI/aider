d15d81f93f691d4a10d4d230466dc0c30c49962d
AssafKoren <60843589+AssafKoren@users.noreply.github.com>
2023-07-07 01:29:35 +0300

    Adding comments to main.py file

--- aider/main.py
+++ aider/main.py
@@ -1,3 +1,4 @@
+# Importing necessary Python libraries and custom modules from the 'aider' package.
 import os
 import sys
 
@@ -9,26 +10,40 @@ from aider.coders import Coder
 from aider.io import InputOutput
 
 
+# Function to get the root directory of the current Git repository
+# This function is used to determine the root directory of the Git repository that the script is being run from
 def get_git_root():
     try:
+        # Attempt to find the root directory of the Git repository containing the script.
         repo = git.Repo(search_parent_directories=True)
+        # If successful, return the root directory path.
         return repo.working_tree_dir
     except git.InvalidGitRepositoryError:
+        # If unsuccessful, return None.
         return None
 
 
+# Main function that runs the program
+# This function is the entry point for the script. It takes command line arguments, sets up the necessary objects, and starts the main loop
 def main(args=None, input=None, output=None):
+    # Function to handle command-line arguments, set up the necessary objects, and start the main execution loop.
+
     if args is None:
+        # If no arguments provided, take them from the command line.
         args = sys.argv[1:]
 
+    # Get the root directory of the Git repository containing the script, if it exists.
     git_root = get_git_root()
 
+    # Define the default configuration files.
     default_config_files = [
         os.path.expanduser("~/.aider.conf.yml"),
     ]
+
     if git_root:
         default_config_files.insert(0, os.path.join(git_root, ".aider.conf.yml"))
 
+    # Initialize the argument parser.
     parser = configargparse.ArgumentParser(
         description="aider is GPT powered coding in your terminal",
         add_config_file_help=True,
@@ -37,6 +52,7 @@ def main(args=None, input=None, output=None):
         auto_env_var_prefix="AIDER_",
     )
 
+    # --version option to print the version number of the program.
     parser.add_argument(
         "--version",
         action="version",
@@ -44,6 +60,7 @@ def main(args=None, input=None, output=None):
         help="Show the version number and exit",
     )
 
+    # --config option to specify the configuration file.
     parser.add_argument(
         "-c",
         "--config",
@@ -55,12 +72,15 @@ def main(args=None, input=None, output=None):
         ),
     )
 
+    # Positional argument to accept a list of source code files.
     parser.add_argument(
         "files",
         metavar="FILE",
         nargs="*",
         help="a list of source code files (optional)",
     )
+
+    # Define the default input history file and chat history file.
     default_input_history_file = (
         os.path.join(git_root, ".aider.input.history") if git_root else ".aider.input.history"
     )
@@ -68,24 +88,31 @@ def main(args=None, input=None, output=None):
         os.path.join(git_root, ".aider.chat.history.md") if git_root else ".aider.chat.history.md"
     )
 
+    # --input-history-file option to specify the chat input history file.
     parser.add_argument(
         "--input-history-file",
         metavar="INPUT_HISTORY_FILE",
         default=default_input_history_file,
         help=f"Specify the chat input history file (default: {default_input_history_file})",
     )
+
+    # --chat-history-file option to specify the chat history file.
     parser.add_argument(
         "--chat-history-file",
         metavar="CHAT_HISTORY_FILE",
         default=default_chat_history_file,
         help=f"Specify the chat history file (default: {default_chat_history_file})",
     )
+
+    # --model option to specify the GPT model to use for the main chat.
     parser.add_argument(
         "--model",
         metavar="MODEL",
         default=models.GPT4.name,
         help=f"Specify the model to use for the main chat (default: {models.GPT4.name})",
     )
+
+    # -3 option that is a shortcut for using the GPT3.5 model for the main chat.
     parser.add_argument(
         "-3",
         action="store_const",
@@ -93,24 +120,32 @@ def main(args=None, input=None, output=None):
         const=models.GPT35_16k.name,
         help=f"Use {models.GPT35_16k.name} model for the main chat (gpt-4 is better)",
     )
+
+    # --edit-format option to specify the format that GPT should use for edits.
     parser.add_argument(
         "--edit-format",
         metavar="EDIT_FORMAT",
         default=None,
         help="Specify what edit format GPT should use (default depends on model)",
     )
+
+    # --pretty option to enable pretty, colorized output.
     parser.add_argument(
         "--pretty",
         action="store_true",
         default=True,
         help="Enable pretty, colorized output (default: True)",
     )
+
+    # --no-pretty option to disable pretty, colorized output.
     parser.add_argument(
         "--no-pretty",
         action="store_false",
         dest="pretty",
         help="Disable pretty, colorized output",
     )
+
+    # --no-stream option to disable streaming responses.
     parser.add_argument(
         "--no-stream",
         action="store_false",
@@ -118,6 +153,8 @@ def main(args=None, input=None, output=None):
         default=True,
         help="Disable streaming responses",
     )
+
+    # --no-git option to disable looking for a Git repository.
     parser.add_argument(
         "--no-git",
         action="store_false",
@@ -125,26 +162,36 @@ def main(args=None, input=None, output=None):
         default=True,
         help="Do not look for a git repo",
     )
+
+    # --user-input-color option to set the color for user input.
     parser.add_argument(
         "--user-input-color",
         default="green",
         help="Set the color for user input (default: green)",
     )
+
+    # --tool-output-color option to set the color for tool output.
     parser.add_argument(
         "--tool-output-color",
         default=None,
         help="Set the color for tool output (default: None)",
     )
+
+    # --tool-error-color option to set the color for tool error messages.
     parser.add_argument(
         "--tool-error-color",
         default="red",
         help="Set the color for tool error messages (default: red)",
     )
+
+    # --assistant-output-color option to set the color for assistant output.
     parser.add_argument(
         "--assistant-output-color",
         default="blue",
         help="Set the color for assistant output (default: blue)",
     )
+
+    # --code-theme option to set the markdown code theme.
     parser.add_argument(
         "--code-theme",
         default="default",
@@ -153,11 +200,15 @@ def main(args=None, input=None, output=None):
             " solarized-dark, solarized-light)"
         ),
     )
+
+    # --apply option to apply the changes from a specific file and for debugging.
     parser.add_argument(
         "--apply",
         metavar="FILE",
         help="Apply the changes from the given file instead of running the chat (debug)",
     )
+
+    # --auto-commits option to enable automatic commits of GPT changes.
     parser.add_argument(
         "--auto-commits",
         action="store_true",
@@ -166,12 +217,15 @@ def main(args=None, input=None, output=None):
         help="Enable auto commit of GPT changes (default: True)",
     )
 
+    # --no-auto-commits option to disable automatic commits of GPT changes.
     parser.add_argument(
         "--no-auto-commits",
         action="store_false",
         dest="auto_commits",
         help="Disable auto commit of GPT changes (implies --no-dirty-commits)",
     )
+
+    # --dirty-commits option to enable commits when the repository found dirty.
     parser.add_argument(
         "--dirty-commits",
         action="store_true",
@@ -179,48 +233,64 @@ def main(args=None, input=None, output=None):
         help="Enable commits when repo is found dirty",
         default=True,
     )
+
+    # --no-dirty-commits option to disable commits when the repository found dirty.
     parser.add_argument(
         "--no-dirty-commits",
         action="store_false",
         dest="dirty_commits",
         help="Disable commits when repo is found dirty",
     )
+
+    # The --openai-api-key option to specify the OpenAI API key.
     parser.add_argument(
         "--openai-api-key",
         metavar="OPENAI_API_KEY",
         help="Specify the OpenAI API key",
         env_var="OPENAI_API_KEY",
     )
+
+    # The --openai-api-base option to specify the OpenAI API base endpoint.
     parser.add_argument(
         "--openai-api-base",
         metavar="OPENAI_API_BASE",
         default="https://api.openai.com/v1",
         help="Specify the OpenAI API base endpoint (default: https://api.openai.com/v1)",
     )
+
+    # --dry-run option to perform a dry run without actually applying any changes.
     parser.add_argument(
         "--dry-run",
         action="store_true",
         help="Perform a dry run without applying changes (default: False)",
         default=False,
     )
+
+    # --show-diffs option to show the diffs when committing changes.
     parser.add_argument(
         "--show-diffs",
         action="store_true",
         help="Show diffs when committing changes (default: False)",
         default=False,
     )
+
+    # --map-tokens option to specify the max number of tokens for the repository map.
     parser.add_argument(
         "--map-tokens",
         type=int,
         default=1024,
         help="Max number of tokens to use for repo map, use 0 to disable (default: 1024)",
     )
+
+    # --yes option to always automatically say 'yes' to every confirmation.
     parser.add_argument(
         "--yes",
         action="store_true",
         help="Always say yes to every confirmation",
         default=None,
     )
+
+    # --verbose option to, if activated, enable verbose output.
     parser.add_argument(
         "-v",
         "--verbose",
@@ -228,6 +298,8 @@ def main(args=None, input=None, output=None):
         help="Enable verbose output",
         default=False,
     )
+
+    # --message option to specify a single message to send to GPT, process the reply, and then exit.
     parser.add_argument(
         "--message",
         "--msg",
@@ -235,6 +307,7 @@ def main(args=None, input=None, output=None):
         metavar="COMMAND",
         help="Specify a single message to send GPT, process reply then exit (disables chat mode)",
     )
+
     args = parser.parse_args(args)
 
     io = InputOutput(
@@ -290,9 +363,13 @@ def main(args=None, input=None, output=None):
         use_git=args.git,
     )
 
+    # If the dirty_commits argument is true, the script commits any changes in the repository files
+    # This is useful for keeping track of changes made by the script
     if args.dirty_commits:
         coder.commit(ask=True, which="repo_files")
 
+    # If an apply argument is provided, the script applies the changes from the given file and then exits
+    # This is useful for testing and debugging
     if args.apply:
         with open(args.apply, "r") as f:
             content = f.read()
@@ -300,6 +377,9 @@ def main(args=None, input=None, output=None):
         return
 
     io.tool_output("Use /help to see in-chat commands.")
+
+    # If a message argument is provided, the script sends that message to the GPT model and processes the reply
+    # Otherwise, it starts the main loop of the script
     if args.message:
         io.tool_output()
         coder.run(with_message=args.message)
@@ -307,6 +387,7 @@ def main(args=None, input=None, output=None):
         coder.run()
 
 
+# Point of entry when the script is run from the command line.
 if __name__ == "__main__":
     status = main()
     sys.exit(status)
