b05b8e2087ab0f3a6c5ee01ea01d2a76cad00353
Paul Gauthier <aider@paulg.org>
2023-07-13 13:41:34 -0700

    Improved output when no changes made to git tracked files

--- aider/coders/base_coder.py
+++ aider/coders/base_coder.py
@@ -566,7 +566,7 @@ class Coder:
             )
         else:
             if self.repo:
-                self.io.tool_error("Warning: no changes found in tracked files.")
+                self.io.tool_output("No changes made to git tracked files.")
             saved_message = self.gpt_prompts.files_content_gpt_no_edits
 
         return saved_message
