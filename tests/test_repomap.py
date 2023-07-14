719810b1a15dbdc216de54262e0bcd3470f6f8a1
Paul Gauthier <aider@paulg.org>
2023-07-13 17:47:24 -0700

    removed import

--- tests/test_repomap.py
+++ tests/test_repomap.py
@@ -1,11 +1,24 @@
 import os
+import tempfile
 import unittest
 from unittest.mock import patch
 
 from aider.io import InputOutput
 from aider.repomap import RepoMap
 
-from .utils import IgnorantTemporaryDirectory
+
+class IgnorantTemporaryDirectory:
+    def __init__(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+
+    def __enter__(self):
+        return self.temp_dir.__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.temp_dir.__exit__(exc_type, exc_val, exc_tb)
+        except OSError:
+            pass  # Ignore errors (Windows)
 
 
 class TestRepoMap(unittest.TestCase):
