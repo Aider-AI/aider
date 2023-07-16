a9cca05680d867ec8c3c6663acd11be6e160660b
Paul Gauthier <aider@paulg.org>
2023-07-16 09:39:07 -0300

    Added GitTemporaryDirectory

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,9 @@
+import os
 import tempfile
+
+import git
+
+from aider.dump import dump  # noqa: F401
 
 
 class IgnorantTemporaryDirectory:
@@ -13,3 +18,29 @@ class IgnorantTemporaryDirectory:
             self.temp_dir.__exit__(exc_type, exc_val, exc_tb)
         except OSError:
             pass  # Ignore errors (Windows)
+
+
+class ChdirTemporaryDirectory(IgnorantTemporaryDirectory):
+    def __init__(self):
+        self.cwd = os.getcwd()
+        super().__init__()
+
+    def __enter__(self):
+        res = super().__enter__()
+        os.chdir(self.temp_dir.name)
+        return res
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(self.cwd)
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+
+class GitTemporaryDirectory(ChdirTemporaryDirectory):
+    def __enter__(self):
+        res = super().__enter__()
+
+        repo = git.Repo.init()
+        repo.config_writer().set_value("user", "name", "Test User").release()
+        repo.config_writer().set_value("user", "email", "testuser@example.com").release()
+
+        return res
