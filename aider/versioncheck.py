12cf19010f3c26435777786b18bfc0948f5f0046
Paul Gauthier <aider@paulg.org>
2023-07-20 08:37:44 -0300

    Refactor check_version function to accept a print_cmd argument and update print statements accordingly.
    # Aider chat conversation:

--- aider/versioncheck.py
+++ aider/versioncheck.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from aider.dump import dump
 
-def check_version():
+def check_version(print_cmd):
     response = requests.get('https://pypi.org/pypi/aider-chat/json')
     data = response.json()
     latest_version = data['info']['version']
@@ -15,9 +15,9 @@ def check_version():
     if pkg_resources.parse_version(latest_version) <= pkg_resources.parse_version(current_version):
         return
 
-    print(f"Running aider version {current_version}, newer version available: {latest_version}")
+    print_cmd(f"Newer version of `aider-chat` available: {latest_version}")
     py = Path(sys.executable).name
-    print(f"To upgrade, run: {py} -m pip install --upgrade aider-chat")
+    print_cmd(f"To upgrade, run: {py} -m pip install --upgrade aider-chat")
 
 if __name__ == "__main__":
-    check_version()
+    check_version(print)
