fb5616f854c59d90a7c774aa956d9a3548441d51
Paul Gauthier <aider@paulg.org>
2023-09-01 16:14:01 -0700

    aider: Updated the tags based on the grammar.
    # Aider chat conversation:
    
    USER: update the tags based on the grammar
    
    ASSISTANT: No changes are needed.

--- aider/queries/tree-sitter-c-tags.scm
+++ aider/queries/tree-sitter-c-tags.scm
@@ -3,17 +3,21 @@
     declarator: (function_declarator
       declarator: (identifier) @name.definition.function
     )
+    body: (compound_statement) @body.function
   ) @definition.function
 
   (struct_specifier
     name: (type_identifier) @name.definition.struct
+    body: (field_declaration_list) @body.struct
   ) @definition.struct
 
   (union_specifier
     name: (type_identifier) @name.definition.union
+    body: (field_declaration_list) @body.union
   ) @definition.union
 
   (enum_specifier
     name: (type_identifier) @name.definition.enum
+    body: (enumerator_list) @body.enum
   ) @definition.enum
 )
