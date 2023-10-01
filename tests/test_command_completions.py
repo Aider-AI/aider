46c69cf96d6459a90b06fbe2d11d094d91021cad
Alexander Kjeldaas <alexander@coinweb.io>
2023-10-01 18:51:34 +0200

    aider: Update expected results for `test_completions_add` in `test_command_completions.py` to an empty list.
    # Aider chat conversation:
    
    USER: I ran this command:
    
      python3 -m unittest tests.test_command_completions
    
    And got this output:
    
    F.
    ======================================================================
    FAIL: test_completions_add (tests.test_command_completions.TestCommandCompletions.test_completions_add)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/Users/alexanderkjeldaas/code/external/aider/tests/test_command_completions.py", line 20, in test_completions_add
        self.assertEqual(result, expected)
    AssertionError: Lists differ: [] != ['a/b/c/d', 'a/b/c/e', 'a/b/c/f']
    
    Second list contains 3 additional elements.
    First extra element 0:
    'a/b/c/d'
    
    - []
    + ['a/b/c/d', 'a/b/c/e', 'a/b/c/f']
    
    ----------------------------------------------------------------------
    Ran 2 tests in 0.000s
    
    FAILED (failures=1)
    Files: {'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd', 'a/b/c/d'}
    Partial: ab/c
    
    ASSISTANT: The test is failing because the `completions_add` method in `aider/command_completions.py` is not returning any matches for the partial string "ab/c".
    
    The `completions_add` method is designed to match the partial string with the filenames in a case-insensitive manner. It does this by checking if all characters in the partial string
    ^C KeyboardInterrupt
    
    USER: update the expected results based on the results in the output
    
    ASSISTANT: Based on the output, it seems like the `completions_add` method is not returning any matches for the partial string "ab/c". Therefore, the expected result should be an empty list.
    
    Here is the change:
    
    ```python
    tests/test_command_completions.py
    <<<<<<< HEAD
        def test_completions_add(self):
            partial = "ab/c"
            result = list(self.command_completions.completions_add(partial))
            expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f']
            self.assertEqual(result, expected)
    =======
        def test_completions_add(self):
            partial = "ab/c"
            result = list(self.command_completions.completions_add(partial))
            expected = []
            self.assertEqual(result, expected)
    >>>>>>> updated
    ```
    
    This change will make the test expect an empty list as the result of the `completions_add` method when the partial string is "ab/c".

--- tests/test_command_completions.py
+++ tests/test_command_completions.py
@@ -16,7 +16,7 @@ class TestCommandCompletions(unittest.TestCase):
     def test_completions_add(self):
         partial = "ab/c"
         result = list(self.command_completions.completions_add(partial))
-        expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f']
+        expected = []
         self.assertEqual(result, expected)
 
     def test_completions_drop(self):
