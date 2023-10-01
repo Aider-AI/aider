4cab191905f8a314671a09f098b683397dd42470
Alexander Kjeldaas <alexander@coinweb.io>
2023-10-01 19:03:07 +0200

    aider: Add test case for partial "aabb" in test_completions_add method.
    # Aider chat conversation:
    
    USER: add another test in test_completions_add where partial = "aabb" where it should only return aa/bb/cc/dd
    
    ASSISTANT: To add another test in the `test_completions_add` method where `partial = "aabb"`, we need to modify the `tests/test_command_completions.py` file.
    
    Here is the step-by-step explanation of the changes:
    
    1. Add a new test case in the `test_completions_add` method.
    2. Set `partial = "aabb"`.
    3. Call the `completions_add` method with the new `partial` and collect the `text` attribute of the returned `Completion` objects in a list.
    4. Sort the resulting list.
    5. Set the `expected` list to `['aa/bb/cc/dd']`.
    6. Compare the `result` list to the `expected` list.
    
    Here is the *edit block* for the change:
    
    ```python
    tests/test_command_completions.py
    <<<<<<< HEAD
        def test_completions_add(self):
            partial = "ab/c"
            result = sorted([completion.text for completion in self.command_completions.completions_add(partial)])
            expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
            self.assertEqual(result, expected)
    =======
        def test_completions_add(self):
            partial = "ab/c"
            result = sorted([completion.text for completion in self.command_completions.completions_add(partial)])
            expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
            self.assertEqual(result, expected)
    
            partial = "aabb"
            result = sorted([completion.text for completion in self.command_completions.completions_add(partial)])
            expected = ['aa/bb/cc/dd']
            self.assertEqual(result, expected)
    >>>>>>> updated
    ```
    This change adds a new test case to the `test_completions_add` method. The new test case checks that when `partial = "aabb"`, the `completions_add` method returns `['aa/bb/cc/dd']`.

--- tests/test_command_completions.py
+++ tests/test_command_completions.py
@@ -19,6 +19,11 @@ class TestCommandCompletions(unittest.TestCase):
         expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
         self.assertEqual(result, expected)
 
+        partial = "aabb"
+        result = sorted([completion.text for completion in self.command_completions.completions_add(partial)])
+        expected = ['aa/bb/cc/dd']
+        self.assertEqual(result, expected)
+
     def test_completions_drop(self):
         partial = "a/b"
         result = list(self.command_completions.completions_drop(partial))
