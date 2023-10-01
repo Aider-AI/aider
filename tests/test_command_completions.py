6c40f5da152e607e650698e02e7a53ce6e1a132d
Alexander Kjeldaas <alexander@coinweb.io>
2023-10-01 18:47:21 +0200

    aider: Added assertions for the returned values from the tested functions in `test_command_completions.py`.
    # Aider chat conversation:
    
    USER: how can I run the tests in test_command_completions?
    
    ASSISTANT: To run the tests in `test_command_completions.py`, you can use the `unittest` module from the command line. Navigate to the directory containing the `tests` folder and run the following command:
    
    ```bash
    python -m unittest tests.test_command_completions
    ```
    
    This command will discover and run all the test methods in the `TestCommandCompletions` class in the `test_command_completions.py` file. The `-m` flag tells Python to run the `unittest` module as a script, and `tests
    ^C KeyboardInterrupt
    
    USER: I ran this command:
    
      python3 -m unittest tests.test_command_completions
    
    And got this output:
    
    ..
    ----------------------------------------------------------------------
    Ran 2 tests in 0.000s
    
    OK
    
    ASSISTANT: Great! The output indicates that both tests
    ^C KeyboardInterrupt
    
    USER: in the tests, use partial="a".  add asserts for the returned values from the tested functions
    
    ASSISTANT: To add assertions for the returned values from the tested functions in `tests/test_command_completions.py`, we need to modify the `test_completions_add` and `test_completions_drop` methods. Here are the *edit blocks* for these changes:
    
    ```python
    tests/test_command_completions.py
    <<<<<<< HEAD
        def test_completions_add(self):
            partial = ""  # Replace with a partial string
            result = list(self.command_completions.completions_add(partial))
            # Add assertions here based on the expected result
    =======
        def test_completions_add(self):
            partial = "a"
            result = list(self.command_completions.completions_add(partial))
            expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
            self.assertEqual(result, expected)
    >>>>>>> updated
    ```
    
    ```python
    tests/test_command_completions.py
    <<<<<<< HEAD
        def test_completions_drop(self):
            partial = ""  # Replace with a partial string
            result = list(self.command_completions.completions_drop(partial))
            # Add assertions here based on the expected result
    =======
        def test_completions_drop(self):
            partial = "a"
            result = list(self.command_completions.completions_drop(partial))
            expected = []
            self.assertEqual(result, expected)
    >>>>>>> updated
    ```
    
    These changes replace the empty strings in `partial = ""` with "a", and add assertions to check that the results of the `completions_add` and `completions_drop` methods are as expected.

--- tests/test_command_completions.py
+++ tests/test_command_completions.py
@@ -14,11 +14,13 @@ class TestCommandCompletions(unittest.TestCase):
         self.command_completions = CommandCompletions(self.coder)
 
     def test_completions_add(self):
-        partial = ""  # Replace with a partial string
+        partial = "a"
         result = list(self.command_completions.completions_add(partial))
-        # Add assertions here based on the expected result
+        expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
+        self.assertEqual(result, expected)
 
     def test_completions_drop(self):
-        partial = ""  # Replace with a partial string
+        partial = "a"
         result = list(self.command_completions.completions_drop(partial))
-        # Add assertions here based on the expected result
+        expected = []
+        self.assertEqual(result, expected)
