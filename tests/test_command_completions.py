080bdd9e445976d8b479664e90635f7b48678e58
Alexander Kjeldaas <alexander@coinweb.io>
2023-10-01 18:47:56 +0200

    aider: Replace `partial = "a"` with `partial = "a/b"` and update the expected results in the `test_completions_add` method.
    # Aider chat conversation:
    
    USER: use partial="a/b"
    
    ASSISTANT: To use `partial="a/b"` in the tests, we need to modify the `test_completions_add` and `test_completions_drop` methods in `tests/test_command_completions.py`. Here are the *edit blocks* for these changes:
    
    ```python
    tests/test_command_completions.py
    <<<<<<< HEAD
        def test_completions_add(self):
            partial = "a"
            result = list(self.command_completions.completions_add(partial))
            expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
            self.assertEqual(result, expected)
    =======
        def test_completions_add(self):
            partial = "a/b"
            result = list(self.command_completions.completions_add(partial))
            expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f']
            self.assertEqual(result, expected)
    >>>>>>> updated
    ```
    
    ```python
    tests/test_command_completions.py
    <<<<<<< HEAD
        def test_completions_drop(self):
            partial = "a"
            result = list(self.command_completions.completions_drop(partial))
            expected = []
            self.assertEqual(result, expected)
    =======
        def test_completions_drop(self):
            partial = "a/b"
            result = list(self.command_completions.completions_drop(partial))
            expected = []
            self.assertEqual(result, expected)
    >>>>>>> updated
    ```
    
    These changes replace `partial = "a"` with `partial = "a/b"`, and update the expected results in the `test_completions_add` method. The `test_completions_drop` method remains the same because there are no files in the chat to drop.

--- tests/test_command_completions.py
+++ tests/test_command_completions.py
@@ -14,13 +14,13 @@ class TestCommandCompletions(unittest.TestCase):
         self.command_completions = CommandCompletions(self.coder)
 
     def test_completions_add(self):
-        partial = "a"
+        partial = "a/b"
         result = list(self.command_completions.completions_add(partial))
-        expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
+        expected = ['a/b/c/d', 'a/b/c/e', 'a/b/c/f']
         self.assertEqual(result, expected)
 
     def test_completions_drop(self):
-        partial = "a"
+        partial = "a/b"
         result = list(self.command_completions.completions_drop(partial))
         expected = []
         self.assertEqual(result, expected)
