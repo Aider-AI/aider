e1e6ea747ca4ef2e11bb8efb64d29bdf44bdd7b5
Alexander Kjeldaas <alexander@coinweb.io>
2023-10-01 18:44:44 +0200

    aider: Added a mocked `Coder` instance with specific return values for `get_all_relative_files` and `get_inchat_relative_files` in the `setUp` method of `TestCommandCompletions` class.
    # Aider chat conversation:
    
    USER: add a mocked Coder instance whith     get_all_relative_files() = [a/b/c/d, a/b/c/e, a/b/c/f, aa/bb/cc/dd] and where get_inchat_relative_files is empty.  use this when initializing CommandCompletions
    
    ASSISTANT: To add a mocked `Coder` instance in the `setUp` method of the `TestCommandCompletions` class in `tests/test_command_completions.py`, we need to modify the `setUp` method. Here is the *edit block* for this change:
    
    ```python
    tests/test_command_completions.py
    <<<<<<< HEAD
        def setUp(self):
            self.coder = None  # Replace with an instance of your coder
            self.command_completions = CommandCompletions(self.coder)
    =======
        def setUp(self):
            class MockCoder:
                def get_all_relative_files(self):
                    return ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
    
                def get_inchat_relative_files(self):
                    return []
    
            self.coder = MockCoder()
            self.command_completions = CommandCompletions(self.coder)
    >>>>>>> updated
    ```
    
    This change replaces the `None` in `self.coder = None` with a mocked `Coder` instance that returns the specified lists when `get_all_relative_files` and `get_inchat_relative_files` are called.

--- tests/test_command_completions.py
+++ tests/test_command_completions.py
@@ -3,7 +3,14 @@ from aider.command_completions import CommandCompletions
 
 class TestCommandCompletions(unittest.TestCase):
     def setUp(self):
-        self.coder = None  # Replace with an instance of your coder
+        class MockCoder:
+            def get_all_relative_files(self):
+                return ['a/b/c/d', 'a/b/c/e', 'a/b/c/f', 'aa/bb/cc/dd']
+
+            def get_inchat_relative_files(self):
+                return []
+
+        self.coder = MockCoder()
         self.command_completions = CommandCompletions(self.coder)
 
     def test_completions_add(self):
