ce8b6523a6af320eec221e266598e0e9f3441b4c
Paul Gauthier <aider@paulg.org>
2023-10-19 12:27:24 -0700

    aider: Refactor struct_specifier, function_declarator, type_definition, and enum_specifier to include the definition class in their respective queries.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- aider/queries/tree-sitter-c-tags.scm
+++ aider/queries/tree-sitter-c-tags.scm
@@ -1,9 +1,9 @@
-(struct_specifier name: (type_identifier) @name body:(_)) @definition.class
+(struct_specifier name: (type_identifier) @name.definition.class body:(_)) @definition.class
 
 (declaration type: (union_specifier name: (type_identifier) @name)) @definition.class
 
-(function_declarator declarator: (identifier) @name) @definition.function
+(function_declarator declarator: (identifier) @name.definition.function) @definition.function
 
-(type_definition declarator: (type_identifier) @name) @definition.type
+(type_definition declarator: (type_identifier) @name.definition.type) @definition.type
 
-(enum_specifier name: (type_identifier) @name) @definition.type
+(enum_specifier name: (type_identifier) @name.definition.type) @definition.type
