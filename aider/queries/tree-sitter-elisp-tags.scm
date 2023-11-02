4e2e03dfefe069a88bc807a95635c2fdf825f6cf
Paul Gauthier <aider@paulg.org>
2023-11-02 09:32:55 -0700

    Pick up refs in elisp

--- aider/queries/tree-sitter-elisp-tags.scm
+++ aider/queries/tree-sitter-elisp-tags.scm
@@ -3,3 +3,6 @@
 
 ;; Treat macros as function definitions for the sake of TAGS.
 (macro_definition name: (symbol) @name.definition.function) @definition.function
+
+;; Match function calls
+(list (symbol) @name.reference.function) @reference.function
