15a1c754a47104303e6d2951fd6a24fb0c674515
Paul Gauthier <aider@paulg.org>
2023-11-06 18:58:24 -0800

    copy

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -196,7 +196,7 @@ def show_stats(dirnames):
         arrowprops={"arrowstyle": "->", "connectionstyle": "arc3,rad=0.3"},
     )
     ax.annotate(
-        "Second attempt,\nincludingn\nunit test errors",
+        "Second attempt,\nincluding\nunit test errors",
         xy=(2.55, 56),
         xytext=(3.5, top),
         horizontalalignment="center",
