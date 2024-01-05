460e12494ca17ea5baee0b7755c761b6c581d8a8
Paul Gauthier <aider@paulg.org>
2024-01-05 14:50:37 -0800

    added 3.12 to pypi classifiers

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python",
         "Topic :: Software Development",
