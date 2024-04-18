f291c8f777ae7175ae8e5a64bf863878d8f03255
Paul Gauthier <aider@paulg.org>
2024-04-18 14:44:50 -0700

    dropped aliases

--- aider/models.py
+++ aider/models.py
@@ -108,17 +108,6 @@ MODEL_SETTINGS = [
     ),
 ]
 
-ALIASES = {
-    # gpt-3.5
-    "gpt-3.5-turbo": "gpt-3.5-turbo-0613",
-    "gpt-3.5-turbo-16k": "gpt-3.5-turbo-16k-0613",
-    # gpt-4
-    "gpt-4-turbo": "gpt-4-turbo-2024-04-09",
-    "gpt-4-turbo-preview": "gpt-4-0125-preview",
-    "gpt-4": "gpt-4-0613",
-    "gpt-4-32k": "gpt-4-32k-0613",
-}
-
 
 class Model:
     name = None
