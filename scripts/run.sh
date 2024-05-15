f1a79f3dd273d67bb9ed25d70c9bb83a27523e95
Paul Gauthier <aider@paulg.org>
2024-05-15 10:57:19 -0700

    added --net host

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the Docker container
-docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it my-jekyll-site
+docker run --rm --network="host" -v "$PWD:/srv/jekyll" -p 4000:4000 --entrypoint /bin/bash -it my-jekyll-site
