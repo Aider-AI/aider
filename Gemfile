46b953ebebfd3028369d034e3ab8a6923c5bd255
Paul Gauthier <aider@paulg.org>
2024-06-05 09:23:28 -0700

    working local build of gh pages

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ gem 'jekyll'
 gem 'jekyll-theme-cayman'
 gem 'jekyll-redirect-from'
 gem 'jekyll-sitemap'
+gem "webrick"
+gem 'github-pages', group: :jekyll_plugins
--- scripts/jekyll_run.sh
+++ scripts/jekyll_run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 # Run the Docker container
-docker run --rm --network="host" -v "$PWD:/srv/jekyll" -p 4000:4000 --entrypoint /bin/bash -it my-jekyll-site
+docker run \
+       --rm \
+       -v "$PWD:/site" \
+       -p 4000:4000 \
+       my-jekyll-site
+
+#       -e HISTFILE=/srv/jekyll/.bash_history \
+#       --entrypoint /bin/bash \
+#       -it \
