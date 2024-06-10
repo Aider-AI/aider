ce46ea8d62931d94461f03fdc78a8c714e086e20
Paul Gauthier <aider@paulg.org>
2024-06-10 15:33:49 -0700

    moved tests/ to aider/tests/

--- aider/tests/test_urls.py
+++ aider/tests/test_urls.py
@@ -1,8 +1,14 @@
 import requests
+
 from aider import urls
 
+
 def test_urls():
-    url_attributes = [attr for attr in dir(urls) if not callable(getattr(urls, attr)) and not attr.startswith("__")]
+    url_attributes = [
+        attr
+        for attr in dir(urls)
+        if not callable(getattr(urls, attr)) and not attr.startswith("__")
+    ]
     for attr in url_attributes:
         url = getattr(urls, attr)
         response = requests.get(url)
