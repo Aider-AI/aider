9c214e0faf9ccbab96a610e763f14396b46541a0
Paul Gauthier <aider@paulg.org>
2024-06-27 10:21:04 -0700

    Implemented option to attribute commit messages in GitRepo class.

--- aider/repo.py
+++ aider/repo.py
@@ -25,12 +25,14 @@ class GitRepo:
         models=None,
         attribute_author=True,
         attribute_committer=True,
+        attribute_commit_message=False,
     ):
         self.io = io
         self.models = models
 
         self.attribute_author = attribute_author
         self.attribute_committer = attribute_committer
+        self.attribute_commit_message = attribute_commit_message
 
         if git_dname:
             check_fnames = [git_dname]
