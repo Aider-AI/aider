2a506d4d1581a9a426abdad7148a6d932a33786b
Paul Gauthier <aider@paulg.org>
2024-07-05 13:30:18 -0300

    update jekyll to aider/website/

--- scripts/jekyll_run.sh
+++ scripts/jekyll_run.sh
@@ -3,7 +3,7 @@
 # Run the Docker container
 docker run \
        --rm \
-       -v "$PWD/website:/site" \
+       -v "$PWD/aider/website:/site" \
        -p 4000:4000 \
        -e HISTFILE=/site/.bash_history \
        -it \
