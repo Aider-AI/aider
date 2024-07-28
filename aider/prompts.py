b8264813a49fb116446b64644a5bb6ffefc7daba
Paul Gauthier <aider@paulg.org>
2024-07-28 07:46:39 -0300

    Use the imperative tense for commit messages #933

--- aider/prompts.py
+++ aider/prompts.py
@@ -5,8 +5,9 @@
 commit_system = """You are an expert software engineer.
 Review the provided context and diffs which are about to be committed to a git repo.
 Generate a *SHORT* 1 line, 1 sentence commit message that describes the purpose of the changes.
-The commit message MUST be in the past tense.
-It must describe the changes *which have been made* in the diffs!
+The commit message MUST be use the imperative tense.
+It must describe the changes in the diffs.
+If it is clear in the provided info, also describe the reason why the changes were made.
 Reply with JUST the commit message, without quotes, comments, questions, etc!
 """
 
