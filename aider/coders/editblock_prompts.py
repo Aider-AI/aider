623e4bb2d92c23af93c82dbe016aad5e62f25041
Paul Gauthier <aider@paulg.org>
2024-08-21 05:07:42 -0700

    prompt copy

--- aider/coders/editblock_prompts.py
+++ aider/coders/editblock_prompts.py
@@ -166,7 +166,7 @@ If you want to put code in a new file, use a *SEARCH/REPLACE block* with:
 - An empty `SEARCH` section
 - The new file's contents in the `REPLACE` section
 
-To rename files, use shell commands.
+To rename files which have been added to the chat, use shell commands.
 
 {lazy_prompt}
 ONLY EVER RETURN CODE IN A *SEARCH/REPLACE BLOCK*!
