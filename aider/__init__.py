aee501d1281883fe721341f515c6cd941b17dd18
Paul Gauthier <aider@paulg.org>
2024-08-30 11:39:59 -0700

    feat: add git hash to development version

--- aider/__init__.py
+++ aider/__init__.py
@@ -1,1 +1,16 @@
 __version__ = "0.54.8-dev"
+
+"""
+[[[cog
+# TODO: write python to emit `git_hash = "X"`, where X is the latest 7-char git hash
+# python goes here
+]]]
+"""
+git_hash = "deadbee"
+"""
+[[[end]]]
+"""
+
+if '-dev' in __version__:
+    xyz = __version__.split("-")[0]
+    __version__ = xyz + "-dev+" + git_hash
