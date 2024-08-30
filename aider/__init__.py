9a24b3c6d012aeb8200d206c3cf60fe088671d83
Paul Gauthier <aider@paulg.org>
2024-08-30 11:47:55 -0700

    stamp the version with the current git hash

--- aider/__init__.py
+++ aider/__init__.py
@@ -1,22 +1,21 @@
 __version__ = "0.54.8-dev"
 
-"""
-[[[cog
+"""[[[cog
 import subprocess
 
 try:
-    git_hash = subprocess.check_output(['git', 'rev-parse', '--short=7', 'HEAD'], universal_newlines=True).strip()
+    git_hash = subprocess.check_output(
+        ['git', 'rev-parse', '--short=7', 'HEAD'],
+        universal_newlines=True,
+        ).strip()
 except subprocess.CalledProcessError:
-    git_hash = ""
+    git_hash = None
 
-print(f'git_hash = "{git_hash}"')
-]]]
-"""
-git_hash = "deadbee"  # This line will be replaced by cog
-"""
-[[[end]]]
-"""
+cog.out(f'git_hash = "{git_hash}"')
+]]]"""
+git_hash = "3fcb9e2"
+"""[[[end]]]"""
 
-if '-dev' in __version__:
+if "-dev" in __version__ and git_hash:
     xyz = __version__.split("-")[0]
     __version__ = xyz + "-dev+" + git_hash
