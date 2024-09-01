2b92be1e349633c4fe3ccd335d9428d9f0afc930
Paul Gauthier <aider@paulg.org>
2024-08-31 18:00:32 -0700

    set version to 0.54.10-dev

--- aider/__init__.py
+++ aider/__init__.py
@@ -6,6 +6,6 @@ except Exception:
     try:
         from aider.__version__ import __version__
     except Exception:
-        __version__ = "0.54.9"
+        __version__ = "0.54.10-dev"
 
 __all__ = [__version__]
