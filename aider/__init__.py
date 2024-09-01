e6078357d23a770444dbea7466a72c632ff64b57
Paul Gauthier <aider@paulg.org>
2024-09-01 07:39:22 -0700

    do not use setuptools_scm at runtime

--- aider/__init__.py
+++ aider/__init__.py
@@ -1,11 +1,6 @@
-from setuptools_scm import get_version
-
 try:
-    __version__ = get_version(root="..", relative_to=__file__)
+    from aider.__version__ import __version__
 except Exception:
-    try:
-        from aider.__version__ import __version__
-    except Exception:
-        __version__ = "0.54.11-dev"
+    __version__ = "0.54.11-dev"
 
 __all__ = [__version__]
