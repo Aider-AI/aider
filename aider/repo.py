00f42590c87fc4082f4e7c62efff9abaeec85d58
Paul Gauthier <aider@paulg.org>
2024-09-09 13:41:28 -0700

    feat: add AssertionError to ANY_GIT_ERROR tuple

--- aider/repo.py
+++ aider/repo.py
@@ -10,7 +10,7 @@ from aider.sendchat import simple_send_with_retries
 
 from .dump import dump  # noqa: F401
 
-ANY_GIT_ERROR = (git.exc.ODBError, git.exc.GitError, OSError)
+ANY_GIT_ERROR = (git.exc.ODBError, git.exc.GitError, OSError, AssertionError)
 
 
 class GitRepo:
