999ec8237c01626b254f05a37f0b8ca1c8bb7002
Elifarley <elifarley@gmail.com>
2024-10-23 11:04:03 -0300

    Update benchmark_diff_analysis.py
    Improvements in naming and docs

--- benchmark/benchmark_diff_analysis.py
+++ benchmark/benchmark_diff_analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
-This script compares two test report files and analyzes the changes in test performance.
-It categorizes tests as improved, worsened, stable, or present in only one of the reports.
+This script compares two benchmark run dir and analyzes the changes in test performance.
+It categorizes tests as improved, worsened, stable, or present in only one of the benchmark runs.
 """
 import os
 import subprocess
@@ -69,15 +69,15 @@ def create_aider_test_result(csv_string):
     return AiderTestResult(**converted_values)
 
 
-def parse_report(benchmark_dir: str) -> list[AiderTestResult]:
+def parse_benchmark_dir(benchmark_dir: str) -> list[AiderTestResult]:
     """
-    Parse a report file and extract test results.
+    Parse a benchmark run dir and extract test results.
 
     Args:
-    benchmark_dir (str): Path to the report file.
+    benchmark_dir (str): Path to the benchmark run dir.
 
     Returns:
-    dict[str, int]: A dictionary where keys are test names and values are the number of failed runs.
+    list[AiderTestResult]: A list of test resulkts
 
     The function reads the file line by line, looking for lines that start with a number or a minus sign.
     These lines are expected to be in the format: "failed_attempts,test_name".
@@ -92,24 +92,24 @@ def parse_report(benchmark_dir: str) -> list[AiderTestResult]:
     return results
 
 
-def compare_test_results(benchmark_run_1: dict[str, AiderTestResult], benchmark_run_2: dict[str, AiderTestResult]) -> tuple[list[str], list[str], list[str], list[str], list[str]]:
+def compare_benchmark_runs(benchmark_run_1: dict[str, AiderTestResult], benchmark_run_2: dict[str, AiderTestResult]) -> tuple[list[str], list[str], list[str], list[str], list[str]]:
     """
-    Compare two test reports and categorize the changes.
+    Compare two benchmark run dirs and categorize the changes.
 
     Args:
-    benchmark_run_1 (dict[str, int]): First report, where keys are test names and values are failed attempt counts.
-    benchmark_run_2 (dict[str, int]): Second report, in the same format as benchmark_run_1.
+    benchmark_run_1 (dict[str, AiderTestResult]): First  test result from first benchmark run, where keys are test names.
+    benchmark_run_2 (dict[str, AiderTestResult]): Second test result from first benchmark run, in the same format as above.
 
     Returns:
     tuple[list[str], list[str], list[str], list[str], list[str]]: A tuple containing lists of:
+        - tests only in benchmark_run_1
+        - tests only in benchmark_run_2
         - improved tests
         - worsened tests
         - stable tests
-        - tests only in benchmark_run_1
-        - tests only in benchmark_run_2
 
-    Tests are categorized based on their presence in the reports and changes in failed attempt counts.
-    Negative failed run counts are treated specially, indicating a different kind of failure.
+    Tests are categorized based on their presence in the runs and changes in failed attempt counts.
+    Negative failed run counts indicate the limit of failed attempts was reached and the test didn't pass.
     """
     only_1 = []
     only_2 = []
@@ -153,106 +153,105 @@ def compare_test_results(benchmark_run_1: dict[str, AiderTestResult], benchmark_
     return only_1, only_2, improved, worsened, stable
 
 
-def main(file_path1: str, file_path2: str):
+def main(benchmark_dir_1: str, benchmark_dir_2: str):
     """
-    Main function to compare two test report files and print the analysis.
+    Main function to compare two benchmark runs and print the analysis.
 
     Args:
-    file_path1 (str): Path to the first report file.
-    file_path2 (str): Path to the second report file.
+    benchmark_dir_1 (str): Path to the first benchmark run.
+    benchmark_dir_2 (str): Path to the second benchmark run.
 
-    This function parses both report files, compares them, and prints a detailed analysis
-    of how tests have changed between the two reports. It categorizes tests as improved,
-    worsened, stable, or present in only one report, and provides a summary count for each category.
+    This function parses both benchmark runs, compares them, and prints a detailed analysis
+    of how tests have changed between the two runs. It categorizes tests as improved,
+    worsened, stable, or present in only one run, and provides a summary count for each category and sub-category.
     """
-    print(f"--- {file_path1.split('/')[-1]}")
-    print(f"+++ {file_path2.split('/')[-1]}")
+    print(f"--- {benchmark_dir_1.split('/')[-1]}")
+    print(f"+++ {benchmark_dir_2.split('/')[-1]}")
     print("# ============= Failed Attempts per Test =============")
     print("# N >= 0: It eventually passed after N failed attempts")
     print("# N < 0 : All attempts failed and the limit was reached")
-
-    test_result_1 = {t.name: t for t in parse_report(file_path1)}
-    test_result_2 = {t.name: t for t in parse_report(file_path2)}
+    benchmark_run_1 = {t.name: t for t in parse_benchmark_dir(benchmark_dir_1)}
+    benchmark_run_2 = {t.name: t for t in parse_benchmark_dir(benchmark_dir_2)}
 
     (
         test_names_only_1, test_names_only_2, test_names_improved, test_names_worsened, test_names_stable
-    ) = compare_test_results(test_result_1, test_result_2)
+    ) = compare_benchmark_runs(benchmark_run_1, benchmark_run_2)
 
-    test_names_only_1_passed = [t for t in test_names_only_1 if test_result_1[t].failed_attempt_count >= 0]
+    test_names_only_1_passed = [t for t in test_names_only_1 if benchmark_run_1[t].failed_attempt_count >= 0]
     if test_names_only_1_passed:
         print()
         print(f"@@ REMOVED ({len(test_names_only_1_passed)} PASSED) @@")
         for test_name in test_names_only_1_passed:
-            failed_attempt_count = test_result_1[test_name].failed_attempt_count
+            failed_attempt_count = benchmark_run_1[test_name].failed_attempt_count
             print(f"<{'-' if failed_attempt_count < 0 else '+'}{test_name}: {failed_attempt_count}")
 
-    test_names_only_1_failed = [t for t in test_names_only_1 if test_result_1[t].failed_attempt_count < 0]
+    test_names_only_1_failed = [t for t in test_names_only_1 if benchmark_run_1[t].failed_attempt_count < 0]
     if test_names_only_1_failed:
         print()
         print(f"@@ REMOVED ({len(test_names_only_1_failed)} FAILED) @@")
         for test_name in test_names_only_1_failed:
-            failed_attempt_count = test_result_1[test_name].failed_attempt_count
+            failed_attempt_count = benchmark_run_1[test_name].failed_attempt_count
             print(f"<{'-' if failed_attempt_count < 0 else '+'}{test_name}: {failed_attempt_count}")
 
-    test_names_only_2_passed = [t for t in test_names_only_2 if test_result_2[t].failed_attempt_count >= 0]
+    test_names_only_2_passed = [t for t in test_names_only_2 if benchmark_run_2[t].failed_attempt_count >= 0]
     if test_names_only_2_passed:
         print()
         print(f"@@ NEW ({len(test_names_only_2_passed)} PASSED) @@")
         for test_name in test_names_only_2_passed:
-            failed_attempt_count = test_result_2[test_name].failed_attempt_count
+            failed_attempt_count = benchmark_run_2[test_name].failed_attempt_count
             print(f">{'-' if failed_attempt_count < 0 else '+'}{test_name}: {failed_attempt_count}")
 
-    test_names_only_2_failed = [t for t in test_names_only_2 if test_result_2[t].failed_attempt_count < 0]
+    test_names_only_2_failed = [t for t in test_names_only_2 if benchmark_run_2[t].failed_attempt_count < 0]
     if test_names_only_2_failed:
         print()
         print(f"@@ NEW ({len(test_names_only_2_failed)} FAILED) @@")
         for test_name in test_names_only_2_failed:
-            failed_attempt_count = test_result_2[test_name].failed_attempt_count
+            failed_attempt_count = benchmark_run_2[test_name].failed_attempt_count
             print(f">{'-' if failed_attempt_count < 0 else '+'}{test_name}: {failed_attempt_count}")
 
-    test_names_improved_now_passes = [t for t in test_names_improved if test_result_1[t].failed_attempt_count < 0]
+    test_names_improved_now_passes = [t for t in test_names_improved if benchmark_run_1[t].failed_attempt_count < 0]
     if test_names_improved_now_passes:
         print()
         print(f"@@ Improved, now PASSED ({len(test_names_improved_now_passes)}) @@")
         for test_name in test_names_improved_now_passes:
-            print(f"++{test_name}: {test_result_1.get(test_name).failed_attempt_count} -> {test_result_2.get(test_name).failed_attempt_count}")
+            print(f"++{test_name}: {benchmark_run_1.get(test_name).failed_attempt_count} -> {benchmark_run_2.get(test_name).failed_attempt_count}")
 
-    test_names_improved_minor = [t for t in test_names_improved if test_result_1[t].failed_attempt_count >= 0]
+    test_names_improved_minor = [t for t in test_names_improved if benchmark_run_1[t].failed_attempt_count >= 0]
     if test_names_improved_minor:
         print()
         print(f"@@ Improved, minor ({len(test_names_improved_minor)}) @@")
         for test_name in test_names_improved_minor:
-            print(f"+ {test_name}: {test_result_1.get(test_name).failed_attempt_count} -> {test_result_2.get(test_name).failed_attempt_count}")
+            print(f"+ {test_name}: {benchmark_run_1.get(test_name).failed_attempt_count} -> {benchmark_run_2.get(test_name).failed_attempt_count}")
 
-    test_names_worsened_now_fails = [t for t in test_names_worsened if test_result_2[t].failed_attempt_count < 0]
+    test_names_worsened_now_fails = [t for t in test_names_worsened if benchmark_run_2[t].failed_attempt_count < 0]
     if test_names_worsened_now_fails:
         print()
         print(f"@@ Worsened, now FAILED ({len(test_names_worsened_now_fails)}) @@")
         for test_name in test_names_worsened_now_fails:
-            print(f"--{test_name}: {test_result_1.get(test_name).failed_attempt_count} -> {test_result_2.get(test_name).failed_attempt_count}")
+            print(f"--{test_name}: {benchmark_run_1.get(test_name).failed_attempt_count} -> {benchmark_run_2.get(test_name).failed_attempt_count}")
 
-    test_names_worsened_minor = [t for t in test_names_worsened if test_result_2[t].failed_attempt_count >= 0]
+    test_names_worsened_minor = [t for t in test_names_worsened if benchmark_run_2[t].failed_attempt_count >= 0]
     if test_names_worsened_minor:
         print()
         print(f"@@ Worsened, still PASSED ({len(test_names_worsened_minor)}) @@")
         for test_name in test_names_worsened_minor:
-            print(f"- {test_name}: {test_result_1.get(test_name).failed_attempt_count} -> {test_result_2.get(test_name).failed_attempt_count}")
+            print(f"- {test_name}: {benchmark_run_1.get(test_name).failed_attempt_count} -> {benchmark_run_2.get(test_name).failed_attempt_count}")
 
-    test_names_stable_passed = [t for t in test_names_stable if test_result_1[t].failed_attempt_count >= 0]
+    test_names_stable_passed = [t for t in test_names_stable if benchmark_run_1[t].failed_attempt_count >= 0]
     if test_names_stable_passed:
         print()
         print(f"@@ Stable: PASSED ({len(test_names_stable_passed)}) @@")
         for test_name in test_names_stable_passed:
-            failed_attempts_2 = test_result_2.get(test_name).failed_attempt_count
-            print(f"=+{test_name}: {test_result_1.get(test_name).failed_attempt_count}{f" -> {failed_attempts_2}" if failed_attempts_2 is None or failed_attempts_2 < 0 else ''}")
+            failed_attempts_2 = benchmark_run_2.get(test_name).failed_attempt_count
+            print(f"=+{test_name}: {benchmark_run_1.get(test_name).failed_attempt_count}{f" -> {failed_attempts_2}" if failed_attempts_2 is None or failed_attempts_2 < 0 else ''}")
 
-    test_names_stable_failed = [t for t in test_names_stable if test_result_1[t].failed_attempt_count < 0]
+    test_names_stable_failed = [t for t in test_names_stable if benchmark_run_1[t].failed_attempt_count < 0]
     if test_names_stable_failed:
         print()
         print(f"@@ Stable: FAILED ({len(test_names_stable_failed)}) @@")
         for test_name in test_names_stable_failed:
-            failed_attempts_2 = test_result_2.get(test_name).failed_attempt_count
-            print(f"=-{test_name}: {test_result_1.get(test_name).failed_attempt_count}{f" -> {failed_attempts_2}" if failed_attempts_2 is None or failed_attempts_2 < 0 else ''}")
+            failed_attempts_2 = benchmark_run_2.get(test_name).failed_attempt_count
+            print(f"=-{test_name}: {benchmark_run_1.get(test_name).failed_attempt_count}{f" -> {failed_attempts_2}" if failed_attempts_2 is None or failed_attempts_2 < 0 else ''}")
 
     print()
     print("# =============          TOTALS          =============")
@@ -276,12 +275,12 @@ def main(file_path1: str, file_path2: str):
         print(f"# STABLE  : {len(test_names_stable)}")
         print(f"#    PASSED: {len(test_names_stable_passed)}")
         print(f"#    FAILED: {len(test_names_stable_failed)}")
-    test_count_delta = len(test_result_2) - len(test_result_1)
-    if len(test_result_1) == len(test_result_2):
-        print(f"# TOTAL  : {len(test_result_1)}")
+    test_count_delta = len(benchmark_run_2) - len(benchmark_run_1)
+    if len(benchmark_run_1) == len(benchmark_run_2):
+        print(f"# TOTAL  : {len(benchmark_run_1)}")
     else:
         # In case the number of test cases differ between the 2 benchmark runs
-        print(f"# TOTAL  : {len(test_result_1)}{test_count_delta:+}")
+        print(f"# TOTAL  : {len(benchmark_run_1)}{test_count_delta:+}")
 
 
 def benchmark_ls(benchmark_run_dir):
@@ -314,6 +313,6 @@ def benchmark_ls(benchmark_run_dir):
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 3:
-        print("Usage: python benchmark_diff_analysis.py <file_path1> <file_path2>")
+        print("Usage: python benchmark_diff_analysis.py <benchmark_dir_1> <benchmark_dir_2>")
         sys.exit(1)
     main(sys.argv[1], sys.argv[2])
