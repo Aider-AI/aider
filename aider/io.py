129f5fae76164af4c6562f5d4e168b2cac1de225
Paul Gauthier <aider@paulg.org>
2024-11-04 09:09:45 -0800

    cleanup

--- aider/io.py
+++ aider/io.py
@@ -319,10 +319,7 @@ class InputOutput:
             with open(str(filename), "r", encoding=self.encoding) as f:
                 return f.read()
         except OSError as err:
-            import traceback
-
             self.tool_error(f"{filename}: unable to read: {err}")
-            self.tool_error("Traceback:\n" + "".join(traceback.format_stack()))
             return
         except FileNotFoundError:
             self.tool_error(f"{filename}: file not found error")
