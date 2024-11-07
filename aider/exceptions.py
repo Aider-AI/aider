62e93d4002bed4d08447206c29ac378166db89af
Paul Gauthier <aider@paulg.org>
2024-11-07 12:19:41 -0800

    feat: add custom exceptions module

---
+++ aider/exceptions.py
@@ -0,0 +1,35 @@
+
+
+def retry_exceptions():
+    import httpx
+    import openai
+
+    return (
+        # httpx
+        httpx.ConnectError,
+        httpx.RemoteProtocolError,
+        httpx.ReadTimeout,
+        #
+        # litellm exceptions inherit from openai exceptions
+        # https://docs.litellm.ai/docs/exception_mapping
+        #
+        # openai.BadRequestError,
+        # litellm.ContextWindowExceededError,
+        # litellm.ContentPolicyViolationError,
+        #
+        # openai.AuthenticationError,
+        # openai.PermissionDeniedError,
+        # openai.NotFoundError,
+        #
+        openai.APITimeoutError,
+        openai.UnprocessableEntityError,
+        openai.RateLimitError,
+        openai.APIConnectionError,
+        # openai.APIError,
+        # openai.APIStatusError,
+        openai.InternalServerError,
+    )
+
+
+class LiteLLMExceptions:
+    # ai
