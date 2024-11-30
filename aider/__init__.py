2439891ee0ab92bfa9e8be6c54cb21263956ce63
Paul Gauthier <aider@paulg.org>
2024-11-30 09:18:17 -0800

    better handle __version__ errors

--- aider/__init__.py
+++ aider/__init__.py
@@ -1,6 +1,17 @@
+from packaging import version
+
+__version__ = "0.65.2.dev"
+safe_version = __version__
+
 try:
     from aider.__version__ import __version__
 except Exception:
-    __version__ = "0.65.2.dev"
+    __version__ = safe_version + ".import"
+
+try:
+    if version.parse(__version__) < version.parse(safe_version):
+        __version__ = safe_version + ".less"
+except Exception:
+    __version__ = safe_version + ".parse"
 
 __all__ = [__version__]
