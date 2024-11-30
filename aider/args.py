c682bd858a9499a1913e40a5e87bf48b79ca0070
Paul Gauthier <aider@paulg.org>
2024-11-30 09:19:01 -0800

    copy

--- aider/args.py
+++ aider/args.py
@@ -559,7 +559,7 @@ def get_parser(default_config_files, git_root):
     group.add_argument(
         "--test",
         action="store_true",
-        help="Run tests and fix problems found",
+        help="Run tests, fix problems found and then exit",
         default=False,
     )
 
