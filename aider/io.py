d57bc3ff01f43ae9221deb9c570437bd84f8b2e4
Paul Gauthier <aider@paulg.org>
2024-12-01 09:41:11 -0800

    chore: remove unused comment in io.py

--- aider/io.py
+++ aider/io.py
@@ -479,7 +479,7 @@ class InputOutput:
                 return ""
             except Exception as err:
                 import traceback
-
+                # remove this comment ai!
                 self.tool_error(str(err))
                 self.tool_error(traceback.format_exc())
                 return ""
