606cd0368f3a2babf5470c321112ecb7342eab9e
Paul Gauthier <paul@aider.chat>
2025-01-04 11:09:55 -0800

    refactor: remove redundant exception handling in GitRepo

--- aider/repo.py
+++ aider/repo.py
@@ -291,11 +291,9 @@ class GitRepo:
                             blob = next(iterator)
                             if blob.type == "blob":  # blob is a file
                                 files.add(blob.path)
-                        except IndexError: 
+                        except IndexError:
                             self.io.tool_warning(f"GitRepo: read error skipping {blob.path}")
                             continue
-                        except ANY_GIT_ERROR as err:
-                            raise err
                         except StopIteration:
                             break
                 except ANY_GIT_ERROR as err:
