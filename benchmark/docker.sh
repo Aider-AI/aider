bdefb1d5fd29df12df2b7924794cbb3997580213
Adrian Cole <adrian.cole@elastic.co>
2025-03-10 17:04:31 +0800

    benchmark: eases configuration of ollama and ENV variables
    Signed-off-by: Adrian Cole <adrian.cole@elastic.co>

--- benchmark/docker.sh
+++ benchmark/docker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# This runs docker with localhost mapped to the host-gateway for convenience.
+# This allows default ENV to work for configuration such as Ollama endponts.
 docker run \
        -it --rm \
        --memory=25g \
        --memory-swap=25g \
-       --add-host=host.docker.internal:host-gateway \
+       --env-file .env \
+       --add-host=localhost:host-gateway \
        -v `pwd`:/aider \
        -v `pwd`/tmp.benchmarks/.:/benchmarks \
-       -e OPENAI_API_KEY=$OPENAI_API_KEY \
        -e HISTFILE=/aider/.bash_history \
        -e PROMPT_COMMAND='history -a' \
        -e HISTCONTROL=ignoredups \
