89c92906023040a8dfa4b6740b17de6f88d6b1a5
Paul Gauthier <paul@aider.chat>
2025-03-18 11:50:10 -0700

    refactor: Simplify asciinema command in tmux_record.sh

--- scripts/tmux_record.sh
+++ scripts/tmux_record.sh
@@ -4,4 +4,5 @@
 TMUX_SIZE=$(tmux display -p '#{window_width}x#{window_height}')
 
 # Start asciinema recording with the tmux window size
-asciinema rec -c "tmux attach -t 0 -r" tmp.cast --headless --append --tty-size $TMUX_SIZE
+asciinema rec -c "tmux attach -t 0 -r" --headless --tty-size $TMUX_SIZE $*
+
