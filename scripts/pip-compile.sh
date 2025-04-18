6eccb9a0815a97c1b037830f2978edc49b59bc1b
Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>
2025-04-18 13:04:16 +0200

    Workaround uv bug to allow more updates

--- scripts/pip-compile.sh
+++ scripts/pip-compile.sh
@@ -6,8 +6,11 @@ set -e
 # Add verbosity flag to see more details about dependency resolution
 VERBOSITY="-v"  # Use -v for less detail, -vvv for even more detail
 
+# Attention: uv doesn't update to minor releases when output-file exists!
+
 # First compile the common constraints of the full requirement suite
 # to make sure that all versions are mutually consistent across files
+rm requirements/common-constraints.txt
 uv pip compile \
     $VERBOSITY \
     --no-strip-extras \
@@ -17,6 +20,7 @@ uv pip compile \
     $1
 
 # Compile the base requirements
+rm tmp.requirements.txt
 uv pip compile \
     $VERBOSITY \
     --no-strip-extras \
@@ -35,6 +39,7 @@ uv pip compile \
 SUFFIXES=(dev help browser playwright)
 
 for SUFFIX in "${SUFFIXES[@]}"; do
+    rm requirements/requirements-${SUFFIX}.txt
     uv pip compile \
         $VERBOSITY \
         --no-strip-extras \
@@ -43,3 +48,10 @@ for SUFFIX in "${SUFFIXES[@]}"; do
         requirements/requirements-${SUFFIX}.in \
         $1
 done
+
+# There were too many different versions required in torch
+# https://github.com/pytorch/pytorch/issues/145225
+# https://github.com/pytorch/pytorch/pull/147197
+# https://github.com/pytorch/pytorch/issues/151312
+sed -i '' 's/^sympy==1.13.1$/sympy==1.13.3/g' requirements/common-constraints.txt requirements/requirements-help.txt
+
