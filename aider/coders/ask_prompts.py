f90b7bfb098579fcc6b9baca00713e5d41ecc358
Paul Gauthier <paul@aider.chat>
2025-05-05 19:02:43 -0700

    better

--- aider/coders/ask_prompts.py
+++ aider/coders/ask_prompts.py
@@ -32,4 +32,4 @@ Here are summaries of some files present in my git repo.
 If you need to see the full contents of any files to answer my questions, ask me to *add them to the chat*.
 """
 
-    system_reminder = ""
+    system_reminder = "{final_reminders}"
