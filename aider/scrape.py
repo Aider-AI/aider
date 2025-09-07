0e7cf3bf45aa52f01fe1be9afd6e33842e1e0f08
Dustin Washington <dwash96@gmail.com>
2025-09-07 11:22:51 -0400

    Install aider-ce in inline pip install commands to account for version un-pinning

--- aider/scrape.py
+++ aider/scrape.py
@@ -42,7 +42,7 @@ def install_playwright(io):
     if has_pip and has_chromium:
         return True
 
-    pip_cmd = utils.get_pip_install(["aider-chat[playwright]"])
+    pip_cmd = utils.get_pip_install(["aider-ce[playwright]"])
     chromium_cmd = "-m playwright install --with-deps chromium"
     chromium_cmd = [sys.executable] + chromium_cmd.split()
 
