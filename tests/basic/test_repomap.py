be8da40b1f050fa325265a75e92b1d7d308679dd
timput <tim@timput.com>
2025-11-02 07:54:12 -0700

    add initial zig-tags.scm for repomap

--- tests/basic/test_repomap.py
+++ tests/basic/test_repomap.py
@@ -334,6 +334,9 @@ class TestRepoMapAllLanguages(unittest.TestCase):
     def test_language_tsx(self):
         self._test_language_repo_map("tsx", "tsx", "UserProps")
 
+    def test_language_zig(self):
+        self._test_language_repo_map("zig", "zig", "add")
+
     def test_language_csharp(self):
         self._test_language_repo_map("csharp", "cs", "IGreeter")
 
