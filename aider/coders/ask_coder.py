927b4b64c19cde963c99ddb7ffa829e14daa0b50
Chris McCormick <chris@mccormick.cx>
2025-11-21 22:12:36 +0800

    Enable shell calls in /ask mode.

--- aider/coders/ask_coder.py
+++ aider/coders/ask_coder.py
@@ -1,5 +1,6 @@
 from .ask_prompts import AskPrompts
 from .base_coder import Coder
+from .editblock_coder import find_original_update_blocks
 
 
 class AskCoder(Coder):
@@ -7,3 +8,14 @@ class AskCoder(Coder):
 
     edit_format = "ask"
     gpt_prompts = AskPrompts()
+
+    def get_edits(self):
+        content = self.partial_response_content
+        try:
+            edits = list(find_original_update_blocks(content, self.fence))
+        except ValueError:
+            return []
+
+        self.shell_commands += [edit[1] for edit in edits if edit[0] is None]
+
+        return []
